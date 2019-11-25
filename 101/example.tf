ea57509afcce5d67d7372a23c3eead2dd011818f
Libert <libert@nuvops.com>
2019-11-25 18:04:18 -0500

    trf cloud

--- 101/example.tf
+++ 101/example.tf
@@ -1,8 +1,17 @@
+terraform {
+  backend "remote" {
+    organization = "nuvops"
+
+    workspaces {
+      name = "101"
+    }
+  }
+}
+
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
-
 resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
@@ -11,10 +20,8 @@ resource "aws_instance" "example" {
 resource "aws_eip" "ip" {
   vpc      = true
   instance = aws_instance.example.id
+
+  provisioner "local-exec" {
+  command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
 }
-
-
-resource "aws_instance" "another" {
-  ami           = "ami-b374d5a5"
-  instance_type = "t2.micro"
 }
